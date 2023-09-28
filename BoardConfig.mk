0ff623502da68de52baa32a03d9736e4be56a9ec
arco <arco68@gmail.com>
2023-09-28 13:19:23 +0000

    sheldon: Silence clang error during kernel compile
    * clang-14: error: unable to execute command: Executable "ld" doesn't exist!
    
    Change-Id: I1ebf0fb665704424a8ad3a375c546c6cca44dc0b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,6 +47,7 @@ TARGET_KERNEL_CONFIG := sheldon_defconfig
 TARGET_KERNEL_CLANG_VERSION := r416183b
 TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/kernel/$(HOST_PREBUILT_TAG)/clang-$(TARGET_KERNEL_CLANG_VERSION)
 TARGET_KERNEL_LLVM_BINUTILS := false
+TARGET_KERNEL_ADDITIONAL_FLAGS := HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # Platform
 TARGET_BOARD_PLATFORM := mt8695
