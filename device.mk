17953e43cecf3e698174decff4f1c53623010832
R0rt1z2 <roger@r0rt1z2.com>
2023-10-01 15:39:27 +0000

    sheldon: Initial device specific overlay
    Change-Id: Ia81b3a874c228c20fd72333301b7a10d1ad93f08

--- device.mk
+++ device.mk
@@ -146,6 +146,9 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
