cd8e0135923a7748e2f7e0ce0c1df2c80e4951af
R0rt1z2 <roger@r0rt1z2.com>
2023-10-20 14:34:53 +0000

    sheldon: Import connectivity RRO overlays
    Change-Id: Ia71886a9c7e70465618dee33eb8879f10270c356

--- device.mk
+++ device.mk
@@ -166,6 +166,10 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+PRODUCT_PACKAGES += \
+    TetheringResOverlaySheldon \
+    WifiResOverlaySheldon
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
