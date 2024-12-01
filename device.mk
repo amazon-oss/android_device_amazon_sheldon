5d19b0695acbc98ea5cf1b295ff565a5118f1522
Tim Zimmermann <tim@linux4.de>
2024-12-01 15:34:38 +0100

    sheldon: Switch to common Pixel libperfmgr

--- device.mk
+++ device.mk
@@ -197,7 +197,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.amazon-libperfmgr
+    android.hardware.power-service.pixel-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
@@ -233,6 +233,8 @@ PRODUCT_SHIPPING_API_LEVEL := 28
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/amazon
 
 # USB
